e3b7cd9a818c0b6b1a085d1e90cddae74560d7b1
SeptiLutfiana <septilutfiana79@gmail>
2025-06-18 13:34:31 +0700

    login by google

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -10,35 +10,44 @@ use Illuminate\Support\Facades\Auth;
 class AuthController extends Controller
 {
     public function __construct()
-{
-    $this->middleware('guest')->except('logout');
-}
-    public function google_redirect(){
+    {
+        $this->middleware('guest')->except('logout');
+    }
+    public function google_redirect()
+    {
         return Socialite::driver('google')->redirect();
     }
-    public function google_callback(){
-        $googleUser = Socialite::driver('google')->user();
+    public function google_callback()
+    {
+        /** @var \Laravel\Socialite\Contracts\User $googleUser */
+        $googleUser = Socialite::driver('google')->stateless()->user();
         $user = User::whereEmail($googleUser->email)->first();
-        if(!$user){
-            $user = User::create(['name'=>$googleUser->name, 'email' =>$googleUser->email, 'status'=>'aktif']);
+
+        if (!$user) {
+            return redirect('/login')->with('failed', 'Email Anda tidak terdaftar di sistem.');
         }
-        if($user && $user->status == 'banded'){
-            return redirect('/login')->with('failed', 'Akun Anda telah dibekukan');
+
+        if ($user->status == 'banded') {
+            return redirect('/login')->with('failed', 'Akun Anda telah dibekukan.');
         }
-        if($user&& $user->status == 'verify'){
+
+        if ($user->status == 'verify') {
             $user->update(['status' => 'aktif']);
         }
+
         Auth::login($user);
-        if($user->id_role == 1){ 
-            return redirect('/adminsdm-dashboard');
-        } elseif ($user->id_role == 2) {
-            return redirect('/supervisor-dashboard');
-        } elseif ($user->id_role == 3) {
-            return redirect('/mentor-dashboard');
-        } elseif ($user->id_role == 4) {
-            return redirect('/karyawan-dashboard');
-        } else {
-            return redirect('/'); // default jika tidak cocok
-            }
+        session(['active_role' => $user->id_role]);
+        switch ($user->id_role) {
+            case 1:
+                return redirect()->route('adminsdm.dashboard');
+            case 2:
+                return redirect()->route('supervisor.spv-dashboard');
+            case 3:
+                return redirect()->route('mentor.dashboard-mentor');
+            case 4:
+                return redirect()->route('karyawan.dashboard-karyawan');
+            default:
+                return redirect('/');
+        }
     }
 }
